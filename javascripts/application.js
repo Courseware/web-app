7dffa69ee33c9cf503b339a03f30c490b6349d5f
Stas SUȘCOV <stas@net.utcluj.ro>
2013-04-02 02:03:31 +0300

    Update javascripts location.

---
+++ javascripts/application.js
@@ -0,0 +1,14 @@
+//= require vendor/jquery
+
+//= require foundation/foundation.js
+//= require foundation/foundation.alerts.js
+//= require foundation/foundation.dropdown.js
+//= require foundation/foundation.placeholder.js
+//= require foundation/foundation.reveal.js
+//= require foundation/foundation.tooltips.js
+
+//= require handlebars
+//= require ember
+//= require ember-data
+
+//= require_self
